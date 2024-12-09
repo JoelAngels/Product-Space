d07591ae322023c396b4a082913855dafa5a76fe
JoelAngels <joelbaraka4325@gmail.com>
2024-12-09 23:36:33 +0300

    feat: Footer Complete

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,37 @@
+import logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import X from "@/assets/social-x.svg";
+import Insta from "@/assets/social-insta.svg";
+import LinkedIn from "@/assets/social-linkedin.svg";
+import Pin from "@/assets/social-pin.svg";
+import Youtube from "@/assets/social-youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+          <Image src={logo} height={40} alt="Saas Logo" className="relative" />
+        </div>
+        <nav className="flex flex-col gap-6 mt-6 md:flex-row md:justify-center">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <X />
+          <Insta />
+          <LinkedIn />
+          <Pin />
+          <Youtube />
+        </div>
+        <p className="mt-6">
+          &copy; 2024 Angels Group. Inc, All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
